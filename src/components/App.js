debc2fceb5002128ddc2d5a5eb7b016ee1de1a66
Abdela Umer <adheremo65@gmail.com>
2023-06-13 07:24:07 -0400

    react-hook-hogwild

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,12 @@ import Nav from "./Nav";
 import hogs from "../porkers_data";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  //this is a practice challange i have to do when i get a chance
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
